16760724086b06cc554ac727ab6d0b13e94d5934
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2021-10-15 11:00:18 +0300

    .

--- lab3/lab3/lab3.cpp
+++ lab3/lab3/lab3.cpp
@@ -6,28 +6,54 @@ class B1 {
 	int b1;
 public:
 	B1(int x) { b1 = x; }
+	void showB1() {
+		cout << "b1 = " << b1 << endl;
+	}
 };
 
 class B2 {
 	int b2;
 public:
 	B2(int x) { b2 = x; }
+	void showB2() {
+		cout << "b2 = " << b2 << endl;
+	}
 };
 
-class D1 : virtual public B1 {
+class D1 : public B1 {
 	int d1;
 public:
 	D1(int x, int y) : B1(y) { d1 = x; }
+	void showD1() {
+		cout << "d1 = " << d1 << endl;
+		showB1();
+	}
 };
 
 class D2 : private D1 {
 	int d2;
 public:
-	D2(int x, int y, int z) : B1(y), D1(z, y) { d2 = x; }
+	D2(int x, int y, int z) : D1(y, z) { d2 = x; }
+	void showD2() {
+		cout << "d2 = " << d2 << endl;
+		showD1();
+	}
 };
 
 class D3 : public D2, virtual private B2 {
 	int d3;
 public:
-	D3(int x, int y, int z, int i, int j) : B1(y), B2(z), D2(i, y, j) { d3 = x; }
+	D3(int x, int y, int z, int i, int j) : B2(j), D2(y, z, i) { d3 = x; }
+	void showD3() {
+		cout << "d3 = " << d3 << endl;
+		showD2();
+		showB2();
+	}
 };
+
+int main() {
+	D3 obj(4, 7, 2, 1, 5);
+	obj.showD3();
+	system("pause");
+	return 0;
+}
--- lab3/lab3_b/lab3_b.cpp
+++ lab3/lab3_b/lab3_b.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
@@ -22,5 +23,15 @@ protected:
 	int num;
 	bool presence;
 public:
-
+	Library(string a, string b, int c, int d, bool e) : Book(a, b, c), num(d), presence(e) {}
+	void ShowLibrary() {
+		cout << "number of library: " << num << endl;
+		cout << "presence: " << presence << endl;
+	}
 };
+
+int main() {
+	Library book1("Star Wars", "J.Laker", 500, 102344, true);
+	Library book2("Witcher", "I.Zhavets", 218, 2237684, true);
+	Library book3("LoTR", "J.Tolkien", 467, 125630, false);
+}
