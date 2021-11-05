9d4d1e820d9afbaa0b51b9414edb4befe955c21c
Viacheslav Myrhorod <slavik.mirgorod@gmail.com>
2021-11-05 10:34:06 +0200

    .

---
+++ lab7/lab7_a/lab7_a.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std;
+
+class Comp {
+private:
+	double num1;
+	double num2;
+public:
+	void Set(double x, double y) {
+		num1 = x;
+		num2 = y;
+	}
+	void Show() {
+		cout << fixed << num1 << " + " << num2 << " *i\n";
+	}
+};
+
+int main() {
+	srand(time(NULL));
+	double* arr = new double[10];
+	for (int i = 0; i < 10; i++) {
+		arr[i] = ((rand() % 20) + (rand() % 100) / 100);
+		cout << arr[i] << endl;
+	}
+	cout << "\n\n";
+	fstream in("in.txt", ios::binary | ios::out);
+	in.write((char*)arr, sizeof(arr));
+	in.close();
+	in.open("in.txt", ios::binary | ios::in);
+	double temp1, temp2;
+	Comp* numbers = new Comp[5];
+	for (int i = 0; i < 5; i += 2) {
+		in.read((char*)&temp1, sizeof(double));
+		in.read((char*)&temp2, sizeof(double));
+		numbers[i].Set(temp1, temp2);
+	}
+	in.close();
+	for (int i = 0; i < 5; i++)
+		numbers[i].Show();
+	ofstream out("out.txt", ios::binary);
+	out.write((char*)numbers, sizeof(numbers));
+	out.close();
+	delete[] arr;
+	delete[] numbers;
+	system("pause");
+}
